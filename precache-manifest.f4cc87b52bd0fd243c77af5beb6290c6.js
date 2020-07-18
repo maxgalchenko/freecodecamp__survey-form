8b34d04509177aa4bd0e9e2f5b89184c3640c96e
Rainspistols <ffly@mail.ru>
2020-07-18 19:06:28 +0200

    Updates

---
+++ precache-manifest.f4cc87b52bd0fd243c77af5beb6290c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f0f553fadd33807b9d0afe14b2a96ef",
+    "url": "/Rainspistols/freecodecamp__survey-form/index.html"
+  },
+  {
+    "revision": "9c220d1e67e1570a0db2",
+    "url": "/Rainspistols/freecodecamp__survey-form/static/js/2.73c79e3e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Rainspistols/freecodecamp__survey-form/static/js/2.73c79e3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fc75e8380e28c31e6cf",
+    "url": "/Rainspistols/freecodecamp__survey-form/static/js/main.c67a1312.chunk.js"
+  },
+  {
+    "revision": "42035282c916fed8661a",
+    "url": "/Rainspistols/freecodecamp__survey-form/static/js/runtime-main.87065e35.js"
+  },
+  {
+    "revision": "a48f1005c2454fc3bfd2efe895930f81",
+    "url": "/Rainspistols/freecodecamp__survey-form/static/media/homeBg.a48f1005.jpg"
+  }
+]);
